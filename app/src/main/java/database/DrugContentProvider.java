fa91adb92027c754169ad17fe8188dc0b5313bdc
Olu-Olanubi <seyeolanubi@gmail.com>
2018-04-17 18:31:41 +0100

    Implemented core database functionalities using Content Provider

---
+++ app/src/main/java/database/DrugContentProvider.java
@@ -0,0 +1,206 @@
+package database;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static database.Drugs.TABLE_NAME;
+
+public class DrugContentProvider extends ContentProvider {
+
+    //create a DatabaseHelper object
+    private DatabaseHelper mdbHelper;
+
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
+    public static final int TASK_WITH_NAME = 102;
+
+    // CDeclare a static variable for the Uri matcher that you construct
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+// Define a static buildUriMatcher method that associates URI's with their int match
+    /**
+     Initialize a new matcher object without any matches,
+     then use .addURI(String authority, String path, int match) to add matches
+     */
+    public static UriMatcher buildUriMatcher() {
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        /*
+          All paths added to the UriMatcher have a corresponding int.
+          For each kind of uri you may want to access, add the corresponding match with addURI.
+          The two calls below add matches for the task directory and a single item by ID.
+         */
+        uriMatcher.addURI(Drugs.AUTHORITY, Drugs.PATH_DRUG_NAME, TASKS);
+        uriMatcher.addURI(Drugs.AUTHORITY, Drugs.PATH_DRUG_NAME + "/#", TASK_WITH_ID);
+        uriMatcher.addURI(Drugs.AUTHORITY,Drugs.PATH_DRUG_NAME + "/*", TASK_WITH_NAME );
+
+        return uriMatcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mdbHelper = new DatabaseHelper(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        // Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mdbHelper.getReadableDatabase();
+
+        // Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
+        Cursor retCursor;
+
+        // Query for the tasks directory and write a default case
+        switch (match) {
+            // Query for the tasks directory
+            case TASKS:
+                retCursor =  db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case TASK_WITH_ID:
+                retCursor = db.query(Drugs.TABLE_NAME,
+                        new String[]{Drugs.COLUMN_ID, Drugs.COLUMN_NAME, Drugs.COLUMN_DESC, Drugs.COLUMN_INTERVAL, Drugs.COLUMN_START_DATE, Drugs.COLUMN_END_DATE},
+                        Drugs.COLUMN_ID + "=?",
+                        new String[]{String.valueOf(ContentUris.parseId(uri))}, null, null, "ASC");
+                break;
+            case TASK_WITH_NAME:
+                retCursor = db.query(Drugs.TABLE_NAME,
+                        new String[]{Drugs.COLUMN_ID, Drugs.COLUMN_NAME, Drugs.COLUMN_DESC, Drugs.COLUMN_INTERVAL, Drugs.COLUMN_START_DATE, Drugs.COLUMN_END_DATE},
+                        Drugs.COLUMN_NAME + "=?",
+                        new String[]{uri.getLastPathSegment()}, null, null, "ASC");
+                break;
+            // Default exception
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Set a notification URI on the Cursor and return that Cursor
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the desired Cursor
+        return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        // Get access to the task database (to write new data to)
+        final SQLiteDatabase db = mdbHelper.getWritableDatabase();
+        // Write URI matching code to identify the match for the tasks directory
+        int match = sUriMatcher.match(uri);
+        Uri returnUri; // URI to be returned
+        switch (match) {
+            case TASKS:
+                // Insert new values into the database
+                // Inserting values into tasks table
+                long id = db.insert(TABLE_NAME, null, values);
+                if ( id > 0 ) {
+                    returnUri = ContentUris.withAppendedId(Drugs.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            // Set the value for the returnedUri and write the default case for unknown URI's
+            // Default case throws an UnsupportedOperationException
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // Notify the resolver if the uri has been changed, and return the newly inserted URI
+        getContext().getContentResolver().notifyChange(uri, null);
+        // Return constructed uri (this points to the newly inserted row of data)
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        // Get access to the database and write URI matching code to recognize a single item
+        final SQLiteDatabase db = mdbHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+        // Keep track of the number of deleted tasks
+        int tasksDeleted; // starts as 0
+
+        // Write the code to delete a single row of data
+        // [Hint] Use selections to delete an item by its row ID
+        switch (match) {
+            // Handle the single item case, recognized by the ID included in the URI path
+            case TASK_WITH_ID:
+                // Get the task ID from the URI path
+                //String id = uri.getPathSegments().get(1);
+                // Use selections/selectionArgs to filter for this ID
+                tasksDeleted = db.delete(TABLE_NAME, Drugs.COLUMN_ID +  "= ?", new String[]{String.valueOf(ContentUris.parseId(uri))});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Notify the resolver of a change and return the number of items deleted
+        if (tasksDeleted != 0) {
+            // A task was deleted, set notification
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of tasks deleted
+        return tasksDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        SQLiteDatabase db = mdbHelper.getWritableDatabase();
+
+        int drugsUpdated;
+
+        // match code
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            // Handle the single item case, recognized by the ID included in the URI path
+            case TASK_WITH_ID:
+                // Get the task ID from the URI path
+                //String id = uri.getPathSegments().get(1);
+                // Use selections/selectionArgs to filter for this ID
+                drugsUpdated = db.update(Drugs.TABLE_NAME, values, Drugs.COLUMN_ID + " = ?",
+                        new String[] { String.valueOf(ContentUris.parseId(uri)) });
+
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // return number of tasks updated
+        return drugsUpdated;
+
+
+    }
+
+
+
+}
